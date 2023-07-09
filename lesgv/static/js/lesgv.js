b9270e483724204efb0ce174c27070321d67b551
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-09 12:44:12 +0200

    Looking pretty snazzy

--- lesgv/static/js/lesgv.js
+++ lesgv/static/js/lesgv.js
@@ -1,14 +1,16 @@
 let toggle = document.querySelector('.toggle-darkmode');
 
+
 // Turn the theme off if the 'darkmode' key exists in localStorage
 if (localStorage.getItem('darkmode')) {
   document.body.classList.add('darkmode');
   toggle.innerText = 'Nuit';
-} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkmode', true);
-    toggle.innerText = 'Nuit';
-}
+} 
+// else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//     document.body.classList.add('darkmode');
+//     localStorage.setItem('darkmode', true);
+//     toggle.innerText = 'Nuit';
+// }
 
 toggle.addEventListener('click', function(e) {
   e.preventDefault();
