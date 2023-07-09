44cc47e7bc32931f4912de39073f4d5e45122e29
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-09 11:08:08 +0200

    Looking pretty snazzy

--- lesgv/services.py
+++ lesgv/services.py
@@ -2,4 +2,5 @@ import requests
 
 def get_blog_posts():
     response = requests.get('https://blog.lesgrandsvoisins.com/ghost/api/content/posts/?key=1c04bede3eb01bfb3fb106b902&limit=6&include=tags,authors')
-    return response.json()['posts']
+    ret = response.json()['posts']
+    return ret
