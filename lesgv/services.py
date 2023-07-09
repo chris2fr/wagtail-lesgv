3a37842295f2c4bf7fa347e2606c85ac22cb2823
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-09 10:31:25 +0200

    New cards look on blog posts

--- lesgv/services.py
+++ lesgv/services.py
@@ -1,5 +1,5 @@
 import requests
 
 def get_blog_posts():
-    response = requests.get('https://blog.lesgrandsvoisins.com/ghost/api/content/posts/?key=1c04bede3eb01bfb3fb106b902&limit=6')
+    response = requests.get('https://blog.lesgrandsvoisins.com/ghost/api/content/posts/?key=1c04bede3eb01bfb3fb106b902&limit=6&include=tags,authors')
     return response.json()['posts']
