128d0520bf687b85a85d517f00dbcb08d34bb0fd
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-10 15:10:07 +0200

    Added Video

--- lesgv/services.py
+++ lesgv/services.py
@@ -1,5 +1,6 @@
 import requests
 import urllib.parse
+import xmlrpc.client
 
 def get_blog_posts(params={}):
     url = 'https://blog.lesgrandsvoisins.com/ghost/api/content/posts/?key=1c04bede3eb01bfb3fb106b902'
@@ -13,3 +14,7 @@ def get_blog_posts(params={}):
     return ret
 
 
+def get_events(params={}):
+    
+    info = xmlrpc.client.ServerProxy('https://ooo.lesgrandsvoisins.com.com/start').start()
+    url, db, username, password = info['host'], info['database'], info['user'], info['password']
