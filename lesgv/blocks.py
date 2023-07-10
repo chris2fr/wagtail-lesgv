167c017d455a8ab14eb3608693b809c318f3101b
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-10 07:13:55 +0200

    Fixed Block for Blog Posts

--- lesgv/blocks.py
+++ lesgv/blocks.py
@@ -12,12 +12,19 @@ class GhostIndexBlock(blocks.StructBlock):
 
     def get_context(self, value, parent_context=None):
         context = super().get_context(value, parent_context=parent_context)
-        if hasattr(value,'ghost_tag') and value.ghost_tag:
-           if value.ghost_filter:
-              value.ghost_filter += ",primary_tag:{ghost_tag}"
-           else:
-               value.ghost_filter = "primary_tag={ghost_tag}"
-        params = {'ghost_limit':getattr(value,'ghost_limit', 15), 'ghost_include':getattr(value,'ghost_include','tags,authors'),'ghost_order':getattr(value,'ghost_order',""),'ghost_filter':getattr(value,'ghost_filter',""),'ghost_page':"{}".format(getattr(value,'ghost_page',1))}
+        post_filter = value.get('post_filter')
+        if "{}".format(post_filter) != '' and value.get('ghost_tag'):
+            post_filter += ","
+        if value.get('ghost_tag'):
+            post_filter += "primary_tag:{}".format(value.get('ghost_tag'))
+        params = {
+            'ghost_limit':value.get('ghost_limit') or  15, 
+            'ghost_include':value.get('ghost_include') or 'tags,authors',
+            'ghost_order':value.get('ghost_order') or "",
+            'ghost_filter':post_filter,
+            'ghost_page':"{}".format(value.get('ghost_page') or 1)
+        }
+        # print(params)
         context['posts'] = lesgv.services.get_blog_posts(params)
         return context
 
