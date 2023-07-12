5684e7cfa8ce1949e04976862d14267f7b99c158
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-12 08:22:30 +0200

    Added a menu

--- lesgv/static/js/lesgv.js
+++ lesgv/static/js/lesgv.js
@@ -1,5 +1,5 @@
-let toggle = document.querySelector('.toggle-darkmode');
-
+// let toggle = document.querySelector('.toggle-darkmode');
+let toggle = document.querySelector('#jour-nuit');
 
 // Turn the theme off if the 'darkmode' key exists in localStorage
 if (localStorage.getItem('darkmode')) {
@@ -14,7 +14,6 @@ else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 
 toggle.addEventListener('click', function(e) {
   e.preventDefault();
-
   if (document.body.classList.contains('darkmode')) {
     document.body.classList.remove('darkmode');
     toggle.innerText = 'Jour';
@@ -25,3 +24,23 @@ toggle.addEventListener('click', function(e) {
     localStorage.setItem('darkmode', true);
   }
 });
+
+// let menuCheckbox = document.querySelector('#menuToggle');
+// menuCheckbox.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   let menu = document.querySelector('#menu');
+//   if (menu.style.display != "block") {
+//     menu.style.display = "block";
+//   } else {
+//     menu.style.display = "none";
+//   }
+// });
+
+// function toggleMenu(show = false) {
+//   let menu = document.querySelector('#menu');
+//   if (show || menu.style.display != "block") {
+//     menu.style.display = "block";
+//   } else {
+//     menu.style.display = "none";
+//   }
+// }
