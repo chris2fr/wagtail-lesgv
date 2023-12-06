250223d3b9d7abd14bac0429f6323fabfdff562f
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-06 12:22:48 +0100

    trying oauth2 in django

--- lesgv/settings/base.py
+++ lesgv/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     # 'puput',
     'colorful',
     "wagtailmarkdown",
+    "oauth2_provider",
 ]
 
 MIDDLEWARE = [
--- lesgv/urls.py
+++ lesgv/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("search/", search_views.search, name="search"),
     path('wagtail-transfer/', include(wagtailtransfer_urls)),
     # path("__debug__/", include("debug_toolbar.urls")),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
 
 if settings.DEBUG:
